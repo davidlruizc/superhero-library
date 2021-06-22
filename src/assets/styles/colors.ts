799856019f4f2a648252490481d3d6cc0d2d279b
David Ruiz <davidl-ruizc@outlook.com>
2021-06-21 23:09:32 -0500

    adding card color

--- src/assets/styles/colors.ts
+++ src/assets/styles/colors.ts
@@ -5,3 +5,4 @@ export const BACKGROUND_COLOR = '#1D1D1D';
 export const ERROR_COLOR = '#CF6679';
 export const ON_PRIMARY_COLOR = '#000000';
 export const ON_BACKGROUND_COLOR = '#FFFFFF';
+export const BACKGROUND_CARD = '#1F1F1F';
