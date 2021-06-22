68162bf1d7e528413fcc6f63e5dba1912365caae
David Ruiz <davidl-ruizc@outlook.com>
2021-06-21 20:54:57 -0500

    theme colors

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = '#BB86FC';
+export const PRIMARY_VARIANT = '#3700B3';
+export const SECONDARY_COLOR = '#01DAC6';
+export const BACKGROUND_COLOR = '#1D1D1D';
+export const ERROR_COLOR = '#CF6679';
+export const ON_PRIMARY_COLOR = '#000000';
+export const ON_BACKGROUND_COLOR = '#FFFFFF';
