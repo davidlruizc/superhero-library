34a40f35a0b42451ef72046cce7f3cc21ff624c7
David Ruiz <davidl-ruizc@outlook.com>
2021-06-21 23:44:47 -0500

    feed on page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import Content from 'components/molecules/Content';
+import Feed from 'components/molecules/Feed';
 import Header from 'components/molecules/Header';
 import * as React from 'react';
 
@@ -6,6 +7,7 @@ const Home: React.FC = () => {
   return (
     <Content>
       <Header />
+      <Feed />
     </Content>
   );
 };
