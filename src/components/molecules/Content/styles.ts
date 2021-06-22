6c7baed768b65f160a1580c9bad19a7ae8cb9261
David Ruiz <davidl-ruizc@outlook.com>
2021-06-21 23:10:04 -0500

    bg for content

---
+++ src/components/molecules/Content/styles.ts
@@ -0,0 +1,7 @@
+import { BACKGROUND_COLOR } from 'assets/styles/colors';
+import styled from 'styled-components';
+
+export const ContainerWrapper = styled.div`
+  background-color: ${BACKGROUND_COLOR};
+  height: 100vh;
+`;
