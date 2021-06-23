0a4a21a479b1fe8e98afae6529e4e63eec0dc68d
David Ruiz <davidl-ruizc@outlook.com>
2021-06-22 21:42:55 -0500

    clean HTTP acitons

--- src/actions/api/CharacterActions.ts
+++ src/actions/api/CharacterActions.ts
@@ -1,31 +1,5 @@
-import { baseURL, superheroURLs } from 'actions/URLs';
+import { baseURL } from 'actions/URLs';
 import { HttpRequest } from './HttpRequest';
-
-export const Powerstats = async (id: string): Promise<IPowerstatsResponse | undefined> => {
-  try {
-    const request = await new HttpRequest().Get<IPowerstatsResponse>(
-      `${baseURL}/${id}${superheroURLs.powerstats}`
-    );
-    if (request.okay && request.data !== null) {
-      return request.data;
-    }
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
-export const Appearance = async (id: string): Promise<IAppearanceResponse | undefined> => {
-  try {
-    const request = await new HttpRequest().Get<IAppearanceResponse>(
-      `${baseURL}/${id}${superheroURLs.appearance}`
-    );
-    if (request.okay && request.data !== null) {
-      return request.data;
-    }
-  } catch (error) {
-    throw new Error(error);
-  }
-};
 
 export const FindSuperheroById = async (id: string): Promise<ISuperHero | undefined> => {
   try {
